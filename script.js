a4fc9849603d27b28c1100b8cd41b98e31c1ddb8
Biraj Poudel <134138369+notArealdevv@users.noreply.github.com>
2025-05-19 03:03:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 // script.js
 const toggleButton = document.getElementById('toggle-theme');
 
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+=======
+// script.js
+const toggleButton = document.getElementById('toggle-theme');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+>>>>>>> ddc62e2 (Add main.py)
